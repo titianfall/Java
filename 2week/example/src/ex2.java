4c9106112e1dad2cb61ece243b611cc3897563b6
titanfall <qkrwogh7366@gmail.com>
2025-06-24 16:05:59 +0900

    feat : java 상수 대문자로 식별자 선언

---
+++ 2week/example/src/ex2.java
@@ -0,0 +1,10 @@
+public class ex2 {
+    public static void main(String[] args) {
+        final double PI = 3.141592;
+
+        double radius = 10.0;
+        double circleArea = radius * radius * PI;
+
+        System.out.println("원의 면적 = "+circleArea);
+    }
+}
